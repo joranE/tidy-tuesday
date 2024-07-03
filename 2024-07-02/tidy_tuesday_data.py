2d572e161a9fb78ada4bf15e3135860c709f9087
joran <joran.elias@gmail.com>
2024-07-03 15:19:25 -0600

    ok, learning has occurred

--- 2024-07-02/tidy_tuesday_data.py
+++ 2024-07-02/tidy_tuesday_data.py
@@ -3,6 +3,8 @@ import polars.selectors as cs
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_style('whitegrid')
+
 #tt_datasets = pl.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_datasets.csv')
 #tt_summary = pl.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_summary.csv')
 #tt_urls = pl.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_urls.csv')
@@ -19,14 +21,15 @@ tt_urls = pl.read_parquet('2024-07-02/tt_urls.parquet')
 tt_variables = pl.read_parquet('2024-07-02/tt_variables.parquet')
 
 # Scatterplot of variables vs observations
+plt.clf()
+plt.yscale('log')
+plt.xscale('log')
 p = sns.scatterplot(
     data = tt_datasets,
     x = 'variables',
     y = 'observations',
     color = 'blue'
 )
-p.set_yscale('log')
-p.set_xscale('log')
 
 tt_datasets = tt_datasets.with_columns(
     pl.concat_str('year','week',separator='-').alias('year_week') + '-2'
@@ -35,14 +38,13 @@ tt_datasets = tt_datasets.with_columns(
 tt_datasets = tt_datasets.with_columns(
     pl.col('year_week').str.strptime(pl.Date,"%Y-%W-%w",strict = False).alias('date')
 )
-tt_datasets = tt_datasets.with_columns(
-    pl.col('date').str.to_date('%Y-%m-%d').alias('date_date')
-)
 
+plt.clf()
+plt.yscale('log')
+plt.xscale('linear')
 p = sns.lineplot(
     data = tt_datasets,
     x = 'date',
     y = 'observations',
     color = 'blue'
 )
-p.set_yscale('log')
