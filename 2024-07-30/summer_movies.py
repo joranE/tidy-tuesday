f3be06e4207b985a7b4c7acfa0651591eebe53d4
joran <joran.elias@gmail.com>
2024-07-31 14:52:14 -0600

    initial noodling

--- 2024-07-30/summer_movies.py
+++ 2024-07-30/summer_movies.py
@@ -20,5 +20,34 @@ summer_movie_genres = pl.read_parquet(
   source = '2024-07-30/summer_movie_genres.parquet'
 )
 summer_movies = pl.read_parquet(
-  source = '2024-07-30/summer_moveis.parquet'
+  source = '2024-07-30/summer_movies.parquet'
+).with_columns(
+  pl.when(pl.col('tconst') == 'tt9013026')
+    .then(2021)
+    .otherwise(pl.col('year'))
+    .alias('year')
 )
+
+summer_movies.group_by(
+  pl.col('year')
+).agg(
+  pl.col('average_rating').mean().alias('avg_rating')
+).sort(by = 'year')
+
+genre_ratings = summer_movies.join(
+  summer_movie_genres,
+  on = 'tconst'
+)
+
+(
+  p9.ggplot(
+    data = genre_ratings,
+    mapping = p9.aes(x = 'runtime_minutes',y = 'average_rating')
+  ) 
+  + p9.facet_wrap('genres')
+  + p9.geom_point()
+)
+
+genre_ratings.group_by(
+  pl.col('genres')
+).agg()
