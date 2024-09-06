b0c25659e09a193378af50b250519db27a36f6c0
joran <joran.elias@gmail.com>
2024-09-05 20:19:08 -0600

    clean up english monarchs a little

--- 2024-08-20/english_monarchs.py
+++ 2024-08-20/english_monarchs.py
@@ -5,6 +5,6 @@ import plotnine as p9
 p9.theme_set(p9.theme_bw())
 
 english_monarchs = pl.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-08-20/english_monarchs_marriages_df.csv',
-    null_values= ['NA','','N/A'])
+    null_values= ['NA','','N/A','\u2013','?'])
 
 english_monarchs.write_parquet(file = '2024-08-20/english_monarchs.parquet')
