86943779fade104482fe0f1e8f17f34645e879b1
joran <joran.elias@gmail.com>
2024-09-08 18:07:32 -0600

    cleaning english monarch data

--- 2024-08-20/english_monarchs.py
+++ 2024-08-20/english_monarchs.py
@@ -8,3 +8,20 @@ english_monarchs = pl.read_csv('https://raw.githubusercontent.com/rfordatascienc
     null_values= ['NA','','N/A','\u2013','?'])
 
 english_monarchs.write_parquet(file = '2024-08-20/english_monarchs.parquet')
+
+english_monarchs = english_monarchs.with_columns(
+    pl.when(pl.col('year_of_marriage')
+        .str.contains('(?)',literal = True))
+        .then(pl.lit('uncertain'))
+        .alias('unk_flag'),
+    pl.col('year_of_marriage')
+        .str.extract(r"([0-9]+)")
+        .cast(pl.Int16)
+        .alias('marriage_year'),
+    pl.col('king_age')
+        .str.extract(r"([0-9]+)")
+        .cast(pl.Int16)
+        .alias('age')
+)
+
+
